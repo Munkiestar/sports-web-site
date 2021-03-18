dfb16a49812e025ea07e044a692a4fa37ab6312b
Igor <munkieryder@gmail.com>
2021-03-18 13:16:19 +0100

    Navbar + mobile responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.scss";
 
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./Navbar.scss";
+
+import { Switch, Route, Link } from "react-router-dom";
+
+// Material Icons
+import SportsBasketballIcon from "@material-ui/icons/SportsBasketball";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
+
+function Navbar() {
+  // state variables
+  const [click, setClick] = useState(false);
+
+  // toggle the icons
+  const handleClick = () => setClick(!click);
+
+  const handleHamburgerMenu = click ? <CloseIcon /> : <MenuIcon />;
+  const handleActiveLinks = click ? "navbar__links active" : "navbar__links";
+
+  return (
+    <div className="navbar">
+      <div className="navbar__container">
+        <div className="navbar__left">
+          <Link to="/" className="navbar__logo">
+            <h3> Sports</h3>
+            <SportsBasketballIcon className="navbar__iconBasket" />
+          </Link>
+          <div className="navbar__icon" onClick={handleClick}>
+            {handleHamburgerMenu}
+          </div>
+        </div>
+        <div className="navbar__right">
+          <ul className={handleActiveLinks}>
+            <li className="navbar__link">
+              <Link to="/">Početna</Link>
+            </li>
+            <li className="navbar__link">
+              <Link to="/about">O nama</Link>
+            </li>
+            <li className="navbar__link">
+              <Link to="/games">Igre</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
